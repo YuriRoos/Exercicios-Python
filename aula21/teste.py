2c782332e3cd076c32ad02d337643a0a6ee816d9
YuriRoos <yuriroos312@gmail.com>
2022-05-02 21:49:46 -0300

    aula 23

---
+++ aula21/teste.py
@@ -0,0 +1,67 @@
+'''dias=('domingo', 'segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado')
+print(type(dias))
+print(dias[3])'''
+
+
+'''texto='python'
+print(tuple(texto))
+print(texto)'''
+
+
+'''lista=[1,2,3,4]
+lista[2]=8
+print(tuple(lista))'''
+
+
+'''texto='python'
+tuple(texto)
+texto[2]='e'
+print(texto)'''
+
+
+'''lista=[3,5]
+tupla2=(1,2,lista)
+#print(tupla)
+#print(tupla[2])
+#print(len(tupla2))
+print(tupla2.count(2))
+lista=['python', 1, 2, 'java']
+print(lista)'''
+
+
+'''meses=['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+n=1
+while n<4:
+    mes=int(input('escolha um mes [1-12]: '))
+    if 1<=mes<13:
+        print(f'o mes é {meses[mes-1]}')
+    n+=1'''
+
+
+'''meses=['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+#print(meses[-3:])
+#print(meses[:-3])
+#print(meses[3:])
+#print(meses[:3])
+
+#meses.append('Yuri')
+
+#print(meses*2)
+
+meses+=['Yuri' , 'ze']
+print(meses)
+
+for mes in meses:
+    print(mes, end=' ')'''
+
+
+
+
+
+
+
+
+
+
+
+
