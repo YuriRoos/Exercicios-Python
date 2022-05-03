6c96d864d0ec82eac36c8bea2738dcd3cd5d7ba3
YuriRoos <yuriroos312@gmail.com>
2022-05-03 18:58:27 -0300

    aula22

---
+++ aula22/teste1.py
@@ -0,0 +1,46 @@
+lista=['yuri', 'jose', 'maria', 'carlos']
+
+#for n in range(0,len(lista)):
+
+#lista.append('jorge') inserir mais um nome
+
+#lista.insert(0,'jorge') inserir mais um nome na pociçao
+
+#lista.sort() maior pro menor
+
+#lista.sort(reverse=True) menor pro maior
+
+#del lista[-1] #remover o nome da posiçao
+
+#lista.remove('jose') remover pelo nome
+
+#lista.pop() remover o ultimo da lista
+
+lista.clear() #remove todos os nomes
+print(lista)
+#lista.append('jose')
+
+#listaa=lista[:]
+
+#lista.extend(lista)
+
+#print(lista.count('jose')) contagem de quantas vexes o numero apareceu
+
+#print(len(lista)) numero de nomes 
+
+#print(lista.index('yuri')) 
+
+#for indice, nome in enumerate(lista):
+    #print(f'{nome} esta armezenado no indice {indice}') numeraçao dos numeros
+
+#a = [[2,1,-5],[3,7,0],[6,4,8]]
+
+#print(a[0][0]+a[0][1]+a[0][2])
+
+#soma_a=0
+#lin=len(a)
+#col=len(a[0])
+
+#for i in range(lin):
+   # for j in range(col):
+   #     soma_a+=a[i][j]
